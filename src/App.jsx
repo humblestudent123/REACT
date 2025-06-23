4adc8c6d4d0a454823962ccb3e835500df19f4cc
unic <vasickinfedor@gmail.com>
2025-06-23 09:16:14 +0300

    lmao

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ import SignInForm from './components/SignInForm';
 function App() {
   return (
     <div className="div" id='h1'>
-      <h1>Sona</h1>
       <Header />
       <SignInForm />
+      <img src="/images/Sona.png" id='image' alt="" />
     </div>
   );
 }
--- src/components/1comp.jsx
+++ src/components/1comp.jsx
@@ -3,9 +3,7 @@ import '../App.css';
 export default function Header() {
   return (
     <>
-      <div className="div" id='h2'>
-        <h2>Sona — ничего лишнего. Только ты</h2>
-      </div>
+     
       
     </>
   );
